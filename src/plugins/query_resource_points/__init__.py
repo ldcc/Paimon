2e15b2ab16c6536e7dc494629d811bcbe69b9532
ldc <ldcc@live.com>
2021-04-19 12:18:33 +0800

    fixed resource points

--- src/plugins/query_resource_points/__init__.py
+++ src/plugins/query_resource_points/__init__.py
@@ -1,43 +1,41 @@
 from nonebot import on_command, on_endswith
 from nonebot.rule import to_me
-# from .query_resource_points import get_resource_map_mes#, get_resource_list_mes, up_label_and_point_list
+from nonebot.adapters.cqhttp import Event, Bot, Message
+from .query_resource_points import get_resource_map_mes, get_resource_list_mes, up_label_and_point_list
 
 respot = on_endswith('在哪')
 reslist = on_command('素材资源列表', rule=to_me())
 resrefre = on_command('刷新素材资源列表', rule=to_me())
 
 
-# @respot.handle()
-# async def _(bot, event):
-#     res = event.get_message().extract_plain_text().strip()
-#     if res == "":
-#         return
-#
-#     msg = get_resource_map_mes(res[:-2])
-#     print(msg)
-#     await respot.finish(msg)
+@respot.handle()
+async def _(bot, event):
+    res = event.get_message().extract_plain_text().strip()
+    if res == "":
+        return
+
+    msg = get_resource_map_mes(res[:-2])
+    await respot.finish(Message(message=msg))
 
 
-# @reslist.handle()
-# async def _(bot, event):
-#     # 长条消息经常发送失败，所以只能这样了
-#     mes_list = []
-#     txt_list = get_resource_list_mes().split("\n")
-#     for txt in txt_list:
-#         data = {
-#             "type": "node",
-#             "data": {
-#                 "name": "色图机器人",
-#                 "uin": "2854196310",
-#                 "content": txt
-#             }
-#         }
-#         mes_list.append(data)
-#     # await bot.send(event, get_resource_list_mes(), at_sender=True)
-#     await reslist.finish(mes_list)
-#
-#
-# @resrefre.handle()
-# async def _(bot, event):
-#     up_label_and_point_list()
-#     await resrefre.finish('刷新成功')
+@reslist.handle()
+async def _(bot, event):
+    mes_list = []
+    txt_list = get_resource_list_mes().split("\n")
+    for txt in txt_list:
+        data = {
+            "type": "node",
+            "data": {
+                "name": "派蒙",
+                "uin": "2854196310",
+                "content": txt
+            }
+        }
+        mes_list.append(data)
+    await reslist.finish(str(mes_list))
+
+
+@resrefre.handle()
+async def _(bot, event):
+    up_label_and_point_list()
+    await resrefre.finish('刷新成功')
