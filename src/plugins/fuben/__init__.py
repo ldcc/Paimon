ae0455cb3d7cde8238f3cc29f5e39e059eb8036d
ldc <ldcc@live.com>
2021-04-20 17:07:07 +0800

    upgrade fuben uri

--- src/plugins/fuben/__init__.py
+++ src/plugins/fuben/__init__.py
@@ -1,10 +1,10 @@
-import os
 from datetime import datetime
+
 from nonebot.adapters.cqhttp import Message
 from nonebot.rule import to_me
 from nonebot import on_command
 
-fuben_uri = 'https://raw.githubusercontent.com/ldcc/Paimon/master/src/data/fuben'
+# fuben_uri = 'https://raw.githubusercontent.com/ldcc/Paimon/master/src/data/fuben'
 
 zb = on_command('周本', rule=to_me())
 tf = on_command('天赋', rule=to_me())
@@ -13,28 +13,28 @@ we = on_command('武器', rule=to_me())
 
 @zb.handle()
 async def _(bot, event):
-    await zb.finish(message=Message(f'[CQ:image,file={fuben_uri}/zb.png]'))
+    await zb.finish(message=Message('[CQ:image,http://pic.xiaoxuan.xyz:88/image/zb.png]'))
 
 
 @tf.handle()
 async def _(bot, event):
     mes = str(event.get_message())
     if '一' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf1.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf1.png]'))
     elif '二' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf2.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf2.png]'))
     elif '三' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf3.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf3.png]'))
     elif '四' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf4.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf4.png]'))
     elif '五' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf5.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf5.png]'))
     elif '六' in mes:
-        await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf6.png]'))
+        await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf6.png]'))
     else:
         day = str(datetime.now().isoweekday())
         if day != '7':
-            await tf.finish(message=Message(f'[CQ:image,file={fuben_uri}/tf{day}.png]'))
+            await tf.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/tf{day}.png]'))
         else:
             await tf.finish('今天星期天所有副本都可以刷哦！')
 
@@ -43,20 +43,20 @@ async def _(bot, event):
 async def _(bot, event):
     mes = str(event.get_message())
     if '一' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we1.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we1.png]'))
     elif '二' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we2.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we2.png]'))
     elif '三' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we3.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we3.png]'))
     elif '四' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we4.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we4.png]'))
     elif '五' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we5.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we5.png]'))
     elif '六' in mes:
-        await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we6.png]'))
+        await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we6.png]'))
     else:
         day = str(datetime.now().isoweekday())
         if day != '7':
-            await we.finish(message=Message(f'[CQ:image,file={fuben_uri}/we{day}.png]'))
+            await we.finish(message=Message(f'[CQ:image,file=http://pic.xiaoxuan.xyz:88/image/we{day}.png]'))
         else:
             await we.finish('今天星期天所有副本都可以刷哦！')
