d28bab85015b766024448c8565ceeb0b2af01db8
ldc0 <eldhrimnirs@gmail.com>
2021-05-14 17:31:20 +0800

    add cron trigger

--- bot.py
+++ bot.py
@@ -1,36 +1,10 @@
 import nonebot
 from nonebot.adapters.cqhttp import Bot
-from nonebot import require
 
 nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter('cqhttp', Bot)
 nonebot.load_plugins("src/plugins")
-bot_me: Bot
-
-
-@driver.on_bot_connect
-async def _(bot: Bot):
-    global bot_me
-    bot_me = bot
-
-
-@driver.on_bot_disconnect
-async def _(bot: Bot):
-    global bot_me
-    bot_me = None
-
-
-scheduler = require('nonebot_plugin_apscheduler').scheduler
-
-
-@scheduler.scheduled_job('cron', hour='*')
-async def _():
-    if bot_me is not None:
-        groups = await bot_me.get_group_list()
-        for g in groups:
-            await bot_me.send_group_msg(group_id=g['group_id'], message='整点报时咕咕咕')
-
 
 if __name__ == '__main__':
     nonebot.run()
---
+++ src/plugins/cron/__init__.py
@@ -0,0 +1,45 @@
+import base64
+import os
+
+from nonebot import require, get_driver, Bot
+
+driver = get_driver()
+
+bot_me: Bot = None
+FILE_PATH = r'./src/data/cron'
+
+
+@driver.on_bot_connect
+async def _(bot: Bot):
+    global bot_me
+    bot_me = bot
+
+
+@driver.on_bot_disconnect
+async def _(bot: Bot):
+    global bot_me
+    bot_me = None
+
+
+scheduler = require('nonebot_plugin_apscheduler').scheduler
+
+
+@scheduler.scheduled_job('cron', hour='*')
+async def _():
+    if bot_me is not None:
+        groups = await bot_me.get_group_list()
+        for g in groups:
+            await bot_me.send_group_msg(group_id=g['group_id'], message='整点报时咕咕咕')
+
+
+# @scheduler.scheduled_job('cron', hour='15', minute='15')
+@scheduler.scheduled_job('cron', second='*/5')
+async def _():
+    if bot_me is not None:
+        groups = await bot_me.get_group_list()
+        for g in groups:
+            with open(os.path.join(FILE_PATH, '3.j.jpg'), "rb") as j3:
+                encoded_string = base64.b64encode(j3.read())
+                print(encoded_string)
+                print(encoded_string.decode())
+                await bot_me.send_group_msg(group_id='931272376', message='')
