ad5d695b49ebe43d25a8d7cc047e05c7d733a1e0
Ldc <ldcc@live.com>
2021-05-24 10:18:03 +0800

    setu switch

--- src/plugins/ghs/__init__.py
+++ src/plugins/ghs/__init__.py
@@ -1,14 +1,17 @@
-from nonebot import on_command
+from nonebot import on_command, get_driver
 from nonebot.adapters.cqhttp import Bot, Message, Event
 
 from .getPic import ghs_pic3
 
 ghs = on_command('ghs', aliases={'无内鬼', '涩图', '色图', '瑟图'})
-withdraw = on_command('撤回')
 
 
 @ghs.handle()
 async def _(bot: Bot, event: Event):
+    setu_on = get_driver().config.setu_on
+    if not setu_on:
+        return
+
     key = str(event.get_message()).strip()
     pic = await ghs_pic3(key)
     try:
